5f13f6d3cd94c795b013a65932730fb00ccfe83b
carangop <arango.palomino@gmail.com>
2022-08-23 22:22:17 -0500

    ok
    ok

--- src/main/java/com/cuadratura/app/controller/UsuarioController.java
+++ src/main/java/com/cuadratura/app/controller/UsuarioController.java
@@ -7,6 +7,8 @@ import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.data.domain.Page;
@@ -28,14 +30,15 @@ import org.springframework.web.bind.annotation.RestController;
 import com.cuadratura.app.mysql.entity.Usuario;
 import com.cuadratura.app.service.UsuarioService;
 
-
 @RestController
 @RequestMapping("/api")
-public class UsuarioController 
-{
+public class UsuarioController {
+	
+	private static final Logger LOGGER = LogManager.getLogger(UsuarioController.class);
+	
 	@Autowired
 	private UsuarioService usuarioService;
-	
+
 	@Autowired
 	private BCryptPasswordEncoder passwordEncoder;
 
@@ -43,7 +46,7 @@ public class UsuarioController
 	public List<Usuario> index() {
 		return usuarioService.findAll();
 	}
-	
+
 	@GetMapping("/usuarios/page/{page}")
 	public Page<Usuario> index(@PathVariable Integer page) {
 		Pageable pageable = PageRequest.of(page, 4);
@@ -52,66 +55,65 @@ public class UsuarioController
 
 	@GetMapping("/usuarios/{id}")
 	public ResponseEntity<?> show(@PathVariable Long id) {
-		
+
 		Usuario usuario = null;
 		Map<String, Object> response = new HashMap<>();
-		
+
 		try {
 			usuario = usuarioService.findById(id);
-		} catch(DataAccessException e) {
+		} catch (DataAccessException e) {
 			response.put("mensaje", "Error al realizar la consulta en la base de datos");
 			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
 			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		
-		if(usuario == null) {
+
+		if (usuario == null) {
 			response.put("mensaje", "El Usuario ID: ".concat(id.toString().concat(" no existe en la base de datos!")));
 			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
 		}
-		
+
 		return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
 	}
-	
-	/*@GetMapping("/usuarios/filtrarUsuario/{nombreUsername}")
-	//@ResponseStatus(HttpStatus.OK)
-	public List<Usuario> buscarDatos(@PathVariable String nombreUsername) {
-		return usuarioService.findByNameOrLastName(nombreUsername);
-	}*/
-	
-	
+
+	/*
+	 * @GetMapping("/usuarios/filtrarUsuario/{nombreUsername}")
+	 * //@ResponseStatus(HttpStatus.OK) public List<Usuario>
+	 * buscarDatos(@PathVariable String nombreUsername) { return
+	 * usuarioService.findByNameOrLastName(nombreUsername); }
+	 */
+
 	@Secured("ROLE_ADMIN")
 	@PostMapping("/usuarios")
 	public ResponseEntity<?> create(@Valid @RequestBody Usuario usuario, BindingResult result) {
-		
-		passwordEncoder=new BCryptPasswordEncoder();
+
+		passwordEncoder = new BCryptPasswordEncoder();
 		Usuario usuarioNew = null;
 		Map<String, Object> response = new HashMap<>();
-		
-		if(result.hasErrors()) {
 
-			List<String> errors = result.getFieldErrors()
-					.stream()
-					.map(err -> "El campo '" + err.getField() +"' "+ err.getDefaultMessage())
+		if (result.hasErrors()) {
+
+			List<String> errors = result.getFieldErrors().stream()
+					.map(err -> "El campo '" + err.getField() + "' " + err.getDefaultMessage())
 					.collect(Collectors.toList());
-			
+
 			response.put("errors", errors);
 			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
 		}
-		
+
 		try {
 			usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
 			usuarioNew = usuarioService.save(usuario);
-		} catch(DataAccessException e) {
+		} catch (DataAccessException e) {
 			response.put("mensaje", "Error al realizar el insert en la base de datos");
 			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
 			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
 		}
-		
+
 		response.put("mensaje", "El usuario ha sido creado con éxito!");
 		response.put("cliente", usuarioNew);
 		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
 	}
-	
+
 	@Secured("ROLE_ADMIN")
 	@PutMapping("/usuarios/{id}")
 	public ResponseEntity<?> update(@Valid @RequestBody Usuario usuario, BindingResult result, @PathVariable Long id) {
@@ -122,17 +124,16 @@ public class UsuarioController
 
 		Map<String, Object> response = new HashMap<>();
 
-		if(result.hasErrors()) {
+		if (result.hasErrors()) {
 
-			List<String> errors = result.getFieldErrors()
-					.stream()
-					.map(err -> "El campo '" + err.getField() +"' "+ err.getDefaultMessage())
+			List<String> errors = result.getFieldErrors().stream()
+					.map(err -> "El campo '" + err.getField() + "' " + err.getDefaultMessage())
 					.collect(Collectors.toList());
-			
+
 			response.put("errors", errors);
 			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
 		}
-		
+
 		if (usuarioActual == null) {
 			response.put("mensaje", "Error: no se pudo editar, el Usuario ID: "
 					.concat(id.toString().concat(" no existe en la base de datos!")));
