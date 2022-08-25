51f6fda666e98694c38357af6037beb651c9be50
carangop <arango.palomino@gmail.com>
2022-08-25 09:14:58 -0500

    ok
    ok

--- src/main/java/com/cuadratura/app/mysql/entity/CargaPmm.java
+++ src/main/java/com/cuadratura/app/mysql/entity/CargaPmm.java
@@ -6,19 +6,14 @@
 package com.cuadratura.app.mysql.entity;
 
 import java.io.Serializable;
-import java.util.Collection;
 import java.util.Date;
 
 import javax.persistence.Basic;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
