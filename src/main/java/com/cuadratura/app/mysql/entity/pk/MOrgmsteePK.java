72f4a5a693947c57516a57c6854791302e857f84
carangop <arango.palomino@gmail.com>
2022-08-25 23:21:26 -0500

    ok
    ok

--- src/main/java/com/cuadratura/app/mysql/entity/pk/MOrgmsteePK.java
+++ src/main/java/com/cuadratura/app/mysql/entity/pk/MOrgmsteePK.java
@@ -1,7 +1,5 @@
 package com.cuadratura.app.mysql.entity.pk;
 
-
-
 import java.io.Serializable;
 import javax.persistence.Basic;
 import javax.persistence.Column;
@@ -13,14 +11,12 @@ import javax.persistence.Embeddable;
  */
 @Embeddable
 public class MOrgmsteePK implements Serializable {
-    /**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 1L;
 	@Basic(optional = false)
-    @Column(name = "org_lvl_child")
-    private int orgLvlChild;
-    @Basic(optional = false)
-    @Column(name = "org_name_short")
-    private String orgNameShort;
+	@Column(name = "org_lvl_child")
+	private int orgLvlChild;
+	@Basic(optional = false)
+	@Column(name = "org_name_short")
+	private String orgNameShort;
 }
