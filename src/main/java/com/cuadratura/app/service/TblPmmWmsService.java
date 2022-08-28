95fc48f15b521e46c96cb9a5ff0478570e7a277a
carangop <arango.palomino@gmail.com>
2022-08-27 22:41:01 -0500

    ok
    ok

--- src/main/java/com/cuadratura/app/service/TblPmmWmsService.java
+++ src/main/java/com/cuadratura/app/service/TblPmmWmsService.java
@@ -5,5 +5,6 @@ import java.util.List;
 import com.cuadratura.app.oracle.dto.projection.ConsolidadoPmmWmsDto;
 
 public interface TblPmmWmsService {
+	
 	List<ConsolidadoPmmWmsDto> getAllConsolidadoPmmWms( Integer idCargaWms,	Integer idCargaPmm, String idCD);
 }
