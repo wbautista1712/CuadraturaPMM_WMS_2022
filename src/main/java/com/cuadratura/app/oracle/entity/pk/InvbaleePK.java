1ae5c1e5baabec69e780b8fb74d402e661ace55c
carangop <arango.palomino@gmail.com>
2022-08-29 16:57:59 -0500

    ok
    ok

--- src/main/java/com/cuadratura/app/oracle/entity/pk/InvbaleePK.java
+++ src/main/java/com/cuadratura/app/oracle/entity/pk/InvbaleePK.java
@@ -5,7 +5,6 @@ import java.io.Serializable;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
-import javax.persistence.Entity;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
--- src/main/java/com/cuadratura/app/oracle/repository/WmsCinsRepository.java
+++ src/main/java/com/cuadratura/app/oracle/repository/WmsCinsRepository.java
@@ -2,7 +2,6 @@ package com.cuadratura.app.oracle.repository;
 
 import java.util.List;
 
-
 public interface WmsCinsRepository {
-		 List<Object[]> findAllWMSWmsCins() throws Exception;
+	List<Object[]> findAllWMSWmsCins() throws Exception;
 }
