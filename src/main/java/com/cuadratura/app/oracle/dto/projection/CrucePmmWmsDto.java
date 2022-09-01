e559b4e61f0a6531c765f3014a32dcd15f66485f
carangop <arango.palomino@gmail.com>
2022-08-31 21:11:15 -0500

    ok
    ok

--- src/main/java/com/cuadratura/app/oracle/dto/projection/CrucePmmWmsDto.java
+++ src/main/java/com/cuadratura/app/oracle/dto/projection/CrucePmmWmsDto.java
@@ -11,67 +11,85 @@ public class CrucePmmWmsDto {
 	private Integer wms;
 	private Integer sugerenciaAjuste;
 	private Integer sctockBolsaDiscrepancia;
+
 	public Integer getIdcrucePmmWms() {
 		return idcrucePmmWms;
 	}
+
 	public void setIdcrucePmmWms(Integer idcrucePmmWms) {
 		this.idcrucePmmWms = idcrucePmmWms;
 	}
+
 	public Integer getIdTblPmmWms() {
 		return idTblPmmWms;
 	}
+
 	public void setIdTblPmmWms(Integer idTblPmmWms) {
 		this.idTblPmmWms = idTblPmmWms;
 	}
+
 	public String getCd() {
 		return cd;
 	}
+
 	public void setCd(String cd) {
 		this.cd = cd;
 	}
+
 	public Integer getCodMat() {
 		return codMat;
 	}
+
 	public void setCodMat(Integer codMat) {
 		this.codMat = codMat;
 	}
+
 	public String getLote() {
 		return lote;
 	}
+
 	public void setLote(String lote) {
 		this.lote = lote;
 	}
+
 	public String getCruceHomologado() {
 		return cruceHomologado;
 	}
+
 	public void setCruceHomologado(String cruceHomologado) {
 		this.cruceHomologado = cruceHomologado;
 	}
+
 	public Integer getPmm() {
 		return pmm;
 	}
+
 	public void setPmm(Integer pmm) {
 		this.pmm = pmm;
 	}
+
 	public Integer getWms() {
 		return wms;
 	}
+
 	public void setWms(Integer wms) {
 		this.wms = wms;
 	}
+
 	public Integer getSugerenciaAjuste() {
 		return sugerenciaAjuste;
 	}
+
 	public void setSugerenciaAjuste(Integer sugerenciaAjuste) {
 		this.sugerenciaAjuste = sugerenciaAjuste;
 	}
+
 	public Integer getSctockBolsaDiscrepancia() {
 		return sctockBolsaDiscrepancia;
 	}
+
 	public void setSctockBolsaDiscrepancia(Integer sctockBolsaDiscrepancia) {
 		this.sctockBolsaDiscrepancia = sctockBolsaDiscrepancia;
 	}
 
-	
-	
 }
