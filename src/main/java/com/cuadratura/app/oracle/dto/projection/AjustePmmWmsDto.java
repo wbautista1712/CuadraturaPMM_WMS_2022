b1fe34f730114a7feb6738c2ec6f1a48dbe64f19
carangop <arango.palomino@gmail.com>
2022-09-02 20:57:10 -0500

    ok
    ok

--- src/main/java/com/cuadratura/app/oracle/dto/projection/AjustePmmWmsDto.java
+++ src/main/java/com/cuadratura/app/oracle/dto/projection/AjustePmmWmsDto.java
@@ -33,193 +33,253 @@ public class AjustePmmWmsDto {
 	private String wmsEb;
 	private String difEconoBloqueado;
 	private String bolsaDiscrepanciaEcono;
+
 	public Integer getIdcrucePmmWms() {
 		return idcrucePmmWms;
 	}
+
 	public void setIdcrucePmmWms(Integer idcrucePmmWms) {
 		this.idcrucePmmWms = idcrucePmmWms;
 	}
+
 	public Integer getIdTblPmmWms() {
 		return idTblPmmWms;
 	}
+
 	public void setIdTblPmmWms(Integer idTblPmmWms) {
 		this.idTblPmmWms = idTblPmmWms;
 	}
+
 	public String getCd() {
 		return cd;
 	}
+
 	public void setCd(String cd) {
 		this.cd = cd;
 	}
+
 	public Integer getCodMat() {
 		return codMat;
 	}
+
 	public void setCodMat(Integer codMat) {
 		this.codMat = codMat;
 	}
+
 	public String getLote() {
 		return lote;
 	}
+
 	public void setLote(String lote) {
 		this.lote = lote;
 	}
+
 	public Integer getPmmDisponible() {
 		return pmmDisponible;
 	}
+
 	public void setPmmDisponible(Integer pmmDisponible) {
 		this.pmmDisponible = pmmDisponible;
 	}
+
 	public Integer getWmsDisponible() {
 		return wmsDisponible;
 	}
+
 	public void setWmsDisponible(Integer wmsDisponible) {
 		this.wmsDisponible = wmsDisponible;
 	}
+
 	public Integer getDifDispon() {
 		return difDispon;
 	}
+
 	public void setDifDispon(Integer difDispon) {
 		this.difDispon = difDispon;
 	}
+
 	public Integer getPmmEnPuerta() {
 		return pmmEnPuerta;
 	}
+
 	public void setPmmEnPuerta(Integer pmmEnPuerta) {
 		this.pmmEnPuerta = pmmEnPuerta;
 	}
+
 	public Integer getWmsPu() {
 		return wmsPu;
 	}
+
 	public void setWmsPu(Integer wmsPu) {
 		this.wmsPu = wmsPu;
 	}
+
 	public Integer getDifEnpuerta() {
 		return difEnpuerta;
 	}
+
 	public void setDifEnpuerta(Integer difEnpuerta) {
 		this.difEnpuerta = difEnpuerta;
 	}
+
 	public Integer getPmmPerdidoNoDisponible() {
 		return pmmPerdidoNoDisponible;
 	}
+
 	public void setPmmPerdidoNoDisponible(Integer pmmPerdidoNoDisponible) {
 		this.pmmPerdidoNoDisponible = pmmPerdidoNoDisponible;
 	}
+
 	public Integer getWmsPaPd() {
 		return wmsPaPd;
 	}
+
 	public void setWmsPaPd(Integer wmsPaPd) {
 		this.wmsPaPd = wmsPaPd;
 	}
+
 	public Integer getDifPerdido() {
 		return difPerdido;
 	}
+
 	public void setDifPerdido(Integer difPerdido) {
 		this.difPerdido = difPerdido;
 	}
+
 	public Integer getPmmAcondicionaCuarentena() {
 		return pmmAcondicionaCuarentena;
 	}
+
 	public void setPmmAcondicionaCuarentena(Integer pmmAcondicionaCuarentena) {
 		this.pmmAcondicionaCuarentena = pmmAcondicionaCuarentena;
 	}
+
 	public Integer getWmsAcQc() {
 		return wmsAcQc;
 	}
+
 	public void setWmsAcQc(Integer wmsAcQc) {
 		this.wmsAcQc = wmsAcQc;
 	}
+
 	public Integer getDifAcondAcQc() {
 		return difAcondAcQc;
 	}
+
 	public void setDifAcondAcQc(Integer difAcondAcQc) {
 		this.difAcondAcQc = difAcondAcQc;
 	}
+
 	public Integer getPmmCanjeMercaderia() {
 		return pmmCanjeMercaderia;
 	}
+
 	public void setPmmCanjeMercaderia(Integer pmmCanjeMercaderia) {
 		this.pmmCanjeMercaderia = pmmCanjeMercaderia;
 	}
+
 	public Integer getWmsCj() {
 		return wmsCj;
 	}
+
 	public void setWmsCj(Integer wmsCj) {
 		this.wmsCj = wmsCj;
 	}
+
 	public String getDifMercaderiaCj() {
 		return difMercaderiaCj;
 	}
+
 	public void setDifMercaderiaCj(String difMercaderiaCj) {
 		this.difMercaderiaCj = difMercaderiaCj;
 	}
+
 	public Integer getPmmMermas() {
 		return pmmMermas;
 	}
+
 	public void setPmmMermas(Integer pmmMermas) {
 		this.pmmMermas = pmmMermas;
 	}
+
 	public Integer getWmsBj() {
 		return wmsBj;
 	}
+
 	public void setWmsBj(Integer wmsBj) {
 		this.wmsBj = wmsBj;
 	}
+
 	public Integer getDifMermasBj() {
 		return difMermasBj;
 	}
+
 	public void setDifMermasBj(Integer difMermasBj) {
 		this.difMermasBj = difMermasBj;
 	}
+
 	public Integer getBolsaDiscrepancia() {
 		return bolsaDiscrepancia;
 	}
+
 	public void setBolsaDiscrepancia(Integer bolsaDiscrepancia) {
 		this.bolsaDiscrepancia = bolsaDiscrepancia;
 	}
+
 	public String getPmmEconomatoDisponible() {
 		return pmmEconomatoDisponible;
 	}
+
 	public void setPmmEconomatoDisponible(String pmmEconomatoDisponible) {
 		this.pmmEconomatoDisponible = pmmEconomatoDisponible;
 	}
+
 	public String getWmsEc() {
 		return wmsEc;
 	}
+
 	public void setWmsEc(String wmsEc) {
 		this.wmsEc = wmsEc;
 	}
+
 	public String getDifEconoDisponible() {
 		return difEconoDisponible;
 	}
+
 	public void setDifEconoDisponible(String difEconoDisponible) {
 		this.difEconoDisponible = difEconoDisponible;
 	}
+
 	public String getPmmEconomatoBloqueado() {
 		return pmmEconomatoBloqueado;
 	}
+
 	public void setPmmEconomatoBloqueado(String pmmEconomatoBloqueado) {
 		this.pmmEconomatoBloqueado = pmmEconomatoBloqueado;
 	}
+
 	public String getWmsEb() {
 		return wmsEb;
 	}
+
 	public void setWmsEb(String wmsEb) {
 		this.wmsEb = wmsEb;
 	}
+
 	public String getDifEconoBloqueado() {
 		return difEconoBloqueado;
 	}
+
 	public void setDifEconoBloqueado(String difEconoBloqueado) {
 		this.difEconoBloqueado = difEconoBloqueado;
 	}
+
 	public String getBolsaDiscrepanciaEcono() {
 		return bolsaDiscrepanciaEcono;
 	}
+
 	public void setBolsaDiscrepanciaEcono(String bolsaDiscrepanciaEcono) {
 		this.bolsaDiscrepanciaEcono = bolsaDiscrepanciaEcono;
 	}
-	
-	
-	
+
 }
