6c25273477f18f0ea61ef5cdc3f3ff1e88459c9c
carangop <arango.palomino@gmail.com>
2022-09-06 16:54:38 -0500

    ok
    ok

--- src/main/java/com/cuadratura/app/controller/UsuarioController.java
+++ src/main/java/com/cuadratura/app/controller/UsuarioController.java
@@ -83,13 +83,7 @@ public class UsuarioController {
 
 		return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
 	}
-
-	/*
-	 * @GetMapping("/usuarios/filtrarUsuario/{nombreUsername}")
-	 * //@ResponseStatus(HttpStatus.OK) public List<Usuario>
-	 * buscarDatos(@PathVariable String nombreUsername) { return
-	 * usuarioService.findByNameOrLastName(nombreUsername); }
-	 */
+	
 
 	@Secured("ROLE_ADMIN")
 	@PostMapping("/usuarioCreate")
