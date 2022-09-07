028118c6ba87b0ba3dc53b46d4d7efe894c2de48
carangop <arango.palomino@gmail.com>
2022-09-07 08:14:01 -0500

    ok
    ok

--- src/main/java/com/cuadratura/app/mysql/repository/impl/UsuarioRolRepositoryImpl.java
+++ src/main/java/com/cuadratura/app/mysql/repository/impl/UsuarioRolRepositoryImpl.java
@@ -14,29 +14,28 @@ import com.cuadratura.app.mysql.repository.UsuarioRolCustom;
 
 @Repository
 @Transactional
-public class UsuarioRolRepositoryImpl implements   UsuarioRolCustom{
-	
+public class UsuarioRolRepositoryImpl implements UsuarioRolCustom {
+
 	private static final Logger LOGGER = LogManager.getLogger(UsuarioRolRepositoryImpl.class);
-	
 
 	@PersistenceContext(unitName = "jpa_mysql")
 	private EntityManager em;
-	
-	
+
 	@Override
 	public UsuarioRol findUsuarioRol(Integer idUsuario) {
 		StringBuilder sb = new StringBuilder();
 		sb.append(" SELECT * FROM cuadratura.usuario_rol x WHERE x.idUsuario = :idUsuario ");
 
 		LOGGER.info(sb.toString());
-		Query q = this.em.createNativeQuery(sb.toString(), UsuarioRol.class);		
-		q.setParameter("idUsuario",idUsuario);
+		Query q = this.em.createNativeQuery(sb.toString(), UsuarioRol.class);
+		q.setParameter("idUsuario", idUsuario);
 		return (UsuarioRol) q.getSingleResult();
 	}
-	
+
 	public void updateUsuarioRol(Integer idUsuarioRol, Integer idRol) {
-		this.em.createNativeQuery(" UPDATE cuadratura.usuario_rol x  set x.idRol = :idRol WHERE  x.idUsuario_rol  = :idUsuarioRol ").setParameter(1, idRol)
-	      .setParameter(2, idUsuarioRol).executeUpdate();
+		this.em.createNativeQuery(
+				" UPDATE cuadratura.usuario_rol x  set x.idRol = :idRol WHERE  x.idUsuario_rol  = :idUsuarioRol ")
+				.setParameter(1, idRol).setParameter(2, idUsuarioRol).executeUpdate();
 	}
 
 }
