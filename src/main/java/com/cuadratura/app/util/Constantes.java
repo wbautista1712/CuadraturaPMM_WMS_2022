b9212103fff3fad78ba5ef8352e298e6a720c331
carangop <arango.palomino@gmail.com>
2022-09-07 08:11:57 -0500

    ok
    ok

--- src/main/java/com/cuadratura/app/util/Constantes.java
+++ src/main/java/com/cuadratura/app/util/Constantes.java
@@ -1,9 +1,9 @@
 package com.cuadratura.app.util;
 
 public interface Constantes {
-	Integer TIPO_IMPORTACION = 1;
-	Integer ESTADO_CUADRATURA = 1;
-	String USUARIO_CARGA_AUTOMATICO = "AUTOMATICO";
-    public static final String VACIO = "";
-    public static final Boolean ESTADO_ACTIVO = true;
+	public static final Integer TIPO_IMPORTACION = 1;
+	public static final Integer ESTADO_CUADRATURA = 1;
+	public static final String USUARIO_CARGA_AUTOMATICO = "AUTOMATICO";
+	public static final String VACIO = "";
+	public static final Boolean ESTADO_ACTIVO = true;
 }
