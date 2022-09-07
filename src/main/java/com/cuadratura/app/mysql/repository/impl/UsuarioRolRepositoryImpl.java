5b3c1eb59948fffff4e5e396f2a0686b5eff889b
carangop <arango.palomino@gmail.com>
2022-09-07 08:13:28 -0500

    ok
    ok

--- src/main/java/com/cuadratura/app/mysql/repository/impl/UsuarioRolRepositoryImpl.java
+++ src/main/java/com/cuadratura/app/mysql/repository/impl/UsuarioRolRepositoryImpl.java
@@ -26,7 +26,7 @@ public class UsuarioRolRepositoryImpl implements   UsuarioRolCustom{
 	@Override
 	public UsuarioRol findUsuarioRol(Integer idUsuario) {
 		StringBuilder sb = new StringBuilder();
-		sb.append("SELECT * FROM cuadratura.usuario_rol x WHERE x.idUsuario = :idUsuario ");
+		sb.append(" SELECT * FROM cuadratura.usuario_rol x WHERE x.idUsuario = :idUsuario ");
 
 		LOGGER.info(sb.toString());
 		Query q = this.em.createNativeQuery(sb.toString(), UsuarioRol.class);		
@@ -35,7 +35,7 @@ public class UsuarioRolRepositoryImpl implements   UsuarioRolCustom{
 	}
 	
 	public void updateUsuarioRol(Integer idUsuarioRol, Integer idRol) {
-		this.em.createNativeQuery("UPDATE  cuadratura.usuario_rol x  set x.idRol = :idRol WHERE  x.idUsuario_rol  = :idUsuarioRol ").setParameter(1, idRol)
+		this.em.createNativeQuery(" UPDATE cuadratura.usuario_rol x  set x.idRol = :idRol WHERE  x.idUsuario_rol  = :idUsuarioRol ").setParameter(1, idRol)
 	      .setParameter(2, idUsuarioRol).executeUpdate();
 	}
 
