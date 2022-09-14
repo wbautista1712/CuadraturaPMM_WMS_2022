3c433fbfdb9ef71ca57bbcac84ac6daf57553f97
carangop <109491792+carangop@users.noreply.github.com>
2022-09-14 17:22:05 -0500

    o
    ko

--- src/main/java/com/cuadratura/app/mysql/entity/MTipoLogica.java
+++ src/main/java/com/cuadratura/app/mysql/entity/MTipoLogica.java
@@ -6,16 +6,13 @@
 package com.cuadratura.app.mysql.entity;
 
 import java.io.Serializable;
-import java.util.Collection;
 
 import javax.persistence.Basic;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -53,8 +50,7 @@ public class MTipoLogica implements Serializable {
     @Basic(optional = false)
     @Column(name = "estado")
     private boolean estado;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idmTipoLogica")
-    private Collection<AjustePmmWms> ajustePmmWmsCollection;
+
 
     
 }
