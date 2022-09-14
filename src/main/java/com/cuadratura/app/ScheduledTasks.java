290c81ae1e1f664fdd10f0c50c091cb6c2a7d524
carangop <109491792+carangop@users.noreply.github.com>
2022-09-14 11:49:37 -0500

    ok
    ok

--- src/main/java/com/cuadratura/app/ScheduledTasks.java
+++ src/main/java/com/cuadratura/app/ScheduledTasks.java
@@ -9,7 +9,6 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
